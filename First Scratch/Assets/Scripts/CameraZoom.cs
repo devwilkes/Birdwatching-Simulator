f8150b15760b4ba7ae59b61e679d504a9e1eca79
Devon Wilkes <devonowilkes@hotmail.com>
2021-02-22 11:23:37 -0500

    Added CameraZoom Script
    Basic FOV adjustment using the scrollwheel

---
+++ First Scratch/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float minFov = 15f;
+    public float maxFov = 90f;
+    public float sensitivty = 10f;
+
+    /* Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel")* sensitivty;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+}
