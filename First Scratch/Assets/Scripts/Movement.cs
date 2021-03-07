94b21effba0d294583143a5dd683d9053bbe167a
Devon Wilkes <devonowilkes@hotmail.com>
2021-03-06 22:32:51 -0500

    Started making sprint key actions, added UI overlay
    Added additional movement options for playtesting (not sure if sprinting should be implemented in actual gameplay), and added control info in the upper right corner

--- First Scratch/Assets/Scripts/Movement.cs
+++ First Scratch/Assets/Scripts/Movement.cs
@@ -12,20 +12,19 @@ public class Movement : MonoBehaviour{
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
+    bool runPress = false;
 
     void Update(){
 
         isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
-        Debug.Log("debug is " + isGrounded);
+
         if(isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
-        Debug.Log("hinput is " + x);
         float z = Input.GetAxis("Vertical");
-        Debug.Log("zinput is " + z);
-
+        
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move* speed * Time.deltaTime);
@@ -33,5 +32,18 @@ public class Movement : MonoBehaviour{
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        runPress = Input.GetButtonDown("Fire1");
+
+        if(runPress == true){
+            speed += 5f;
+        }
+        else{
+            speed = 12f;
+        }
+
+        Debug.Log(runPress);
+
+
     }
  }
