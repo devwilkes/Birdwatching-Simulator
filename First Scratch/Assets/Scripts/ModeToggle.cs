a78dba56fb277d81954f8db1c7ab3c2282ceeb7c
Devon Wilkes <devonowilkes@hotmail.com>
2021-03-16 21:37:48 -0400

    Added a third person overworld camera!
    Used Cinemachine from the Brackeys third person tutorial for a suitable overworld camera! Will be toggled between 1st and 3rd person with a button after some compiler errors are resolved.

---
+++ First Scratch/Assets/Scripts/ModeToggle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraToggle : MonoBehaviour{
+/*
+var cam1 : Overworld_Camera;
+var cam2 : Camera_Mode_Camera;
+ 
+ function Start(){
+     cam1.enabled = true;
+     cam2.enabled = false;
+ }
+ 
+ function Update(){
+ 
+     if (Input.GetKeyDown(KeyCode.C)) {
+         cam1.enabled = !cam1.enabled;
+         cam2.enabled = !cam2.enabled;
+     }
+ }
+ */
+}
