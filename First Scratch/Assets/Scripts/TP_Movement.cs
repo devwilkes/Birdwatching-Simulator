fa5d494db816591c4ba43ed1e76a63a9b4fd3d1b
Devon Wilkes <devonowilkes@hotmail.com>
2021-03-24 16:52:03 -0400

    Gave TicTac horrible eyes (and other movement stuff)
    Cursed TicTac with eyesight, and ModeToggle partially works for third person movement. toggling works.

--- First Scratch/Assets/Scripts/TP_Movement.cs
+++ First Scratch/Assets/Scripts/TP_Movement.cs
@@ -12,6 +12,13 @@ public class TP_Movement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public float gravity = -9.8f;
+    Vector3 velocity;
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     void Start()
     {
         
@@ -27,12 +34,24 @@ public class TP_Movement : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg * cam.eulerAngles.y;
+            Debug.Log(cam.eulerAngles.y);
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle,
              ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(direction * speed * Time.deltaTime);
+            
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+             //move, then add gravity, and then fall
+            isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+            if(isGrounded && velocity.y < 0){
+                velocity.y = -2f;
+            }
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
         }
+
     }
 }
