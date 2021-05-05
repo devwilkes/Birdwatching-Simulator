d6beaa69f7b6910d0490f7febab0c9ec30b950e7
Devon Wilkes <devonowilkes@hotmail.com>
2021-05-05 13:51:56 -0400

    Menu stuff
    stuff with the menus

---
+++ First Scratch/Assets/Scripts/Menu Scripts/MapUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapUI : MonoBehaviour
+{
+    public Button fastTravel1;
+    public Button fastTravel2;
+    public Button fastTravel3;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void On_FT1_Button_Click(){
+
+    }
+
+        void On_FT2_Button_Click(){
+
+    }
+
+        void On_FT3_Button_Click(){
+
+    }
+}
