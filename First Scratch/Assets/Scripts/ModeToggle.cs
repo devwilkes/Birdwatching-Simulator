42cf6102225cc04a50184b6ca622c3aaddf560c0
Devon Wilkes <devonowilkes@hotmail.com>
2021-05-12 14:45:43 -0400

    HUD stuff!
    Got a first and third person HUD to toggle!

--- First Scratch/Assets/Scripts/ModeToggle.cs
+++ First Scratch/Assets/Scripts/ModeToggle.cs
@@ -1,34 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModeToggle : MonoBehaviour{
 
-public Camera cam1;
-public Camera cam2;
+    public Camera cam1;
+    public Camera cam2;
 
-private GameObject fpControlsUI;
- 
- void Start(){
-     cam1.enabled = true;
-     cam2.enabled = false;
-     GetComponent<FP_Movement>().enabled = true;
-     GetComponent<TP_Movement>().enabled = false;
-     fpControlsUI = GameObject.FindGameObjectWithTag("UI");
-     Debug.Log(fpControlsUI.ToString());
- }
- 
- void Update(){
-     if (Input.GetButtonDown("Fire2")) {
-         cam1.enabled = !cam1.enabled;
-         GetComponent<FP_Movement>().enabled = !cam1.enabled;
-         fpControlsUI.SetActive(false);
-         //moveScript1.enabled = !moveScript1.enabled;
-         cam2.enabled = !cam2.enabled;
-         GetComponent<TP_Movement>().enabled = !cam2.enabled;
-         fpControlsUI.SetActive(true);
-        
-     }
- }
+    public GameObject fpControlsUI;
+    public GameObject tpControlsUI;
+    
+    void Start(){
+        cam1.enabled = true;
+        cam2.enabled = false;
+        GetComponent<FP_Movement>().enabled = true;
+        GetComponent<TP_Movement>().enabled = false;
+        fpControlsUI = GameObject.FindGameObjectWithTag("IGFPUI");
+        tpControlsUI = GameObject.FindGameObjectWithTag("IGTPUI");
+        fpControlsUI.SetActive(false);
+        tpControlsUI.SetActive(true);
+    }
+    
+    void Update(){
+        if (Input.GetButtonDown("Fire2")) {
+            cam1.enabled = !cam1.enabled;
+            GetComponent<FP_Movement>().enabled = !cam1.enabled;
+            tpControlsUI.SetActive(cam1.enabled);
+            cam2.enabled = !cam2.enabled;
+            GetComponent<TP_Movement>().enabled = !cam2.enabled;
+            fpControlsUI.SetActive(cam2.enabled);
+            
+        }
+    }
 
 }
