eeae6ea5365c866d35e145de64d52991af31ccc6
Devon Wilkes <devonowilkes@hotmail.com>
2021-05-12 15:57:14 -0400

    ZOOMSLIDERRRRR
    Tried to get a slider to work for zooming

--- First Scratch/Assets/Scripts/Camera Scripts/CameraZoom.cs
+++ First Scratch/Assets/Scripts/Camera Scripts/CameraZoom.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraZoom : MonoBehaviour
 {
     public float minFov = 15f;
     public float maxFov = 90f;
+
+    private float fovRange;
     public float sensitivty = 10f;
     public Camera mainCam;
 
-    /* Start is called before the first frame update
-    void Start()
+    private Component zoomComponent;
+
+    private Slider zoomSlider;
+
+     void Start()
     {
-        
+         fovRange = maxFov - minFov;
+         Debug.Log(GameObject.Find("First Person Hud") + "is here!");
+         zoomSlider = GameObject.Find("Zoom Slider").GetComponent<Slider>();
     }
-    */
 
     // Update is called once per frame
     void Update(){
@@ -25,10 +32,11 @@ public class CameraZoom : MonoBehaviour
         float fov = mainCam.fieldOfView;
         fov += Input.GetAxis("Mouse ScrollWheel")* sensitivty;
         fov = Mathf.Clamp(fov, minFov, maxFov);
-        mainCam.fieldOfView = fov;
+        if(mainCam.fieldOfView != fov){
+            mainCam.fieldOfView = fov;
+            float sliderValue = (fov - minFov)/fovRange;
+            zoomSlider.SetValueWithoutNotify(sliderValue);
+        }
     }
 
-    void sliderInput(){
-        
-    }
 }
